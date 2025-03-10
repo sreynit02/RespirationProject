e1b3da97f2db3db042a7b89085f01a7eabc09f37
Sreynit Khatt <59736834+sreynit02@users.noreply.github.com>
2025-03-10 12:42:37 -0500

    Update new_python_script.py

--- Assets/CSharp-Scripts/new_python_script.py
+++ Assets/CSharp-Scripts/new_python_script.py
@@ -6,18 +6,18 @@ from gdx import gdx
 import random
 
 #Use this script with C# script UnityWebocketServer.cs
-# gdx = gdx.gdx()
-# gdx.open(connection='ble', device_to_open='GDX-RB 0K501575')
-# gdx.select_sensors([1])
-# gdx.start(100)  # Adjust the rate to 1000ms (1 second)
+gdx = gdx.gdx()
+gdx.open(connection='ble', device_to_open='GDX-RB 0K501575')
+gdx.select_sensors([1])
+gdx.start(100)  # Adjust the rate to 1000ms (1 second)
 
 
 async def send_data():
     uri = "ws://localhost:8000/data"
     async with websockets.connect(uri) as websocket:
         while True:
-            # measurements = gdx.read()
-            measurements = random.randint(0,75)
+            measurements = gdx.read()
+            # measurements = random.randint(0,75)
             if measurements is not None:
                 data = {"value": measurements}
                 try: 
